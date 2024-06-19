6c63cfce16126476b7c0671754fad4b45aa966c9
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-19 18:20:32 -0500

    update - add loop and contador

--- 2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -1,14 +1,26 @@
 //Variables
 let numeroSecreto = 8;
-let numeroUsuario = prompt("Me indicas un numero entre 1 y 10por favor:");
+let numeroUsuario = 0;
+let intentos = 1;
+let palVeces = "vez";
+while(numeroUsuario != numeroSecreto){
+    numeroUsuario = prompt("Me indicas un numero entre 1 y 100000 por favor:");
 
-console.log(numeroUsuario);
-/*
-Este codigo realiza
-la comparacion
-*/
-if (numeroUsuario == numeroSecreto) {
-    alert(`Acertaste, el numero es: ${numeroUsuario}`);
-}else{
-    alert('Lo siento, no acertaste el numero.')
+    console.log(numeroUsuario);
+    /*
+    Este codigo realiza
+    la comparacion
+    */
+    if (numeroUsuario == numeroSecreto) {
+        alert(`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palVeces}`);
+    }else{
+        if(numeroUsuario > numeroSecreto){
+            alert('El numero secreto es menor');
+        }else{
+            alert('El numero secreto es mayor');
+        }
+        //incrementamos el contador cuando no se acierta.
+        intentos += 1
+        palVeces = "veces";
+    }
 }

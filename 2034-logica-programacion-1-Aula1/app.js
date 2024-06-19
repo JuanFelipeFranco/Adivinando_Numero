110fdd998262e805693627eb6687d141879e7095
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-19 17:23:53 -0500

    agregando else

--- 2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -1,8 +1,14 @@
+//Variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+let numeroUsuario = prompt("Me indicas un numero entre 1 y 10por favor:");
 
 console.log(numeroUsuario);
-
+/*
+Este codigo realiza
+la comparacion
+*/
 if (numeroUsuario == numeroSecreto) {
     alert('Acertaste el numero');
+}else{
+    alert('Lo siento, no acertaste el numero.')
 }

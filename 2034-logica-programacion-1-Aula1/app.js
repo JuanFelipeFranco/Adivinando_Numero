1866bedc8fa0e7a4e0e9c93fcd78056467148dd1
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-19 17:33:55 -0500

    add Template String

--- 2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -1,5 +1,5 @@
 //Variables
-let numeroSecreto = 6;
+let numeroSecreto = 8;
 let numeroUsuario = prompt("Me indicas un numero entre 1 y 10por favor:");
 
 console.log(numeroUsuario);
@@ -8,7 +8,7 @@ Este codigo realiza
 la comparacion
 */
 if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
+    alert(`Acertaste, el numero es: ${numeroUsuario}`);
 }else{
     alert('Lo siento, no acertaste el numero.')
 }

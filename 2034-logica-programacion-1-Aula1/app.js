0dd4b57e78e4d06029312f1a7e7eeb3e85b089b3
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-19 19:25:06 -0500

    actualizando el juego

--- 2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -1,18 +1,24 @@
 //Variables
-let numeroSecreto = 8;
+let numeroMaximo = 100;
+let numeroSecreto = Math.floor(Math.random()*numeroMaximo)+1;
 let numeroUsuario = 0;
 let intentos = 1;
-let palVeces = "vez";
-while(numeroUsuario != numeroSecreto){
-    numeroUsuario = prompt("Me indicas un numero entre 1 y 100000 por favor:");
 
-    console.log(numeroUsuario);
+//let palVeces = "vez";
+let maximosIntentos = 3;
+
+console.log(numeroSecreto);
+
+while(numeroUsuario != numeroSecreto){
+    numeroUsuario = parseInt(prompt(`Me indicas un numero entre 1 y ${numeroMaximo} por favor:`));
+
+    console.log(typeof(numeroUsuario));
     /*
     Este codigo realiza
     la comparacion
     */
     if (numeroUsuario == numeroSecreto) {
-        alert(`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palVeces}`);
+        alert(`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
     }else{
         if(numeroUsuario > numeroSecreto){
             alert('El numero secreto es menor');
@@ -20,7 +26,12 @@ while(numeroUsuario != numeroSecreto){
             alert('El numero secreto es mayor');
         }
         //incrementamos el contador cuando no se acierta.
-        intentos += 1
+        intentos ++;
+
         palVeces = "veces";
+        if(intentos > 3){
+            alert(`Llegaste al numero maximo de ${maximosIntentos} intentos`)
+            break;
+        }
     }
 }
